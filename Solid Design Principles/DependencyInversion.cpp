28e1562b9912f6821273b1646e65734a60597dcd
Karan2004xd <prajapatikaran2004@gmail.com>
2023-11-12 17:49:42 +0530

    solid design principles finished

--- Solid Design Principles/DependencyInversion.cpp
+++ Solid Design Principles/DependencyInversion.cpp
@@ -9,23 +9,44 @@ struct Person {
    std::string name;
 };
 
-struct Relationships { // low-level module
+struct RelationshipBrowser {
+   virtual std::vector<Person> find_all_children_of(const std::string &name) = 0;
+};
+
+struct Relationships : RelationshipBrowser { // low-level module
    std::vector<std::tuple<Person, Relations, Person>> relationships;
-   
+
    void add_child_parent(const Person &parent, const Person &child) {
       relationships.push_back({parent, Relations::parent, child});
       relationships.push_back({child, Relations::child, parent});
    }
+   
+   std::vector<Person> find_all_children_of(const std::string &name) override {
+      std::vector<Person> result;
+      for (auto && [first, rel, second] : relationships) {
+         if (name == first.name && rel == Relations::parent) {
+            result.push_back(second);
+         }
+      }
+      return result;
+   }
 };
 
 struct Research { // high-level module
-   Research(const Relationships &obj) {
-      auto &relations = obj.relationships;
-      for (auto && [first, rel, second] : relations) {
+   Research (RelationshipBrowser &browser, const std::string name) {
+      for (auto &i : browser.find_all_children_of(name)) {
+         std::cout << name << " has a child called " << i.name << std::endl;
       }
    }
 };
 
 int main() {
+   Person john {"John"};
+   Person child_1 {"James"}, child_2 {"Chris"};
+   Relationships relationships;
+   relationships.add_child_parent(john, child_1);
+   relationships.add_child_parent(john, child_2);
+   
+   Research _(relationships, "John");
    return 0;
 }
