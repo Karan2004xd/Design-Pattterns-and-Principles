bef5a437badc6e0c13a4104ff744a629e12de5e1
Karan2004xd <prajapatikaran2004@gmail.com>
2023-11-14 22:14:49 +0530

    working on the builder pattern

--- Builder/FluentBuilder.cpp
+++ Builder/FluentBuilder.cpp
@@ -10,8 +10,23 @@ struct HtmlElement {
 
    HtmlElement() {}
    HtmlElement(std::string name, std::string text) : name(name), text(text) {}
-
    
+   std::string str(int indent = 0) const {
+      std::ostringstream oss;
+      std::string i(indent_size * indent, ' ');
+      oss << i << "<" << name << ">" << std::endl;
+
+      if (text.size() > 0) {
+         oss << std::string(indent_size * (indent + 1), ' ') << text << std::endl;
+      }
+
+      for (const auto &e : elements) {
+         oss << e.str(indent + 1);
+      }
+
+      oss << i << "</" << name << ">" << std::endl;
+      return oss.str();
+   }
 };
 
 struct HtmlBuilder {
