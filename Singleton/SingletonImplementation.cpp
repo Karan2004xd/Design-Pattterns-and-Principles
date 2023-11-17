6db09354227174f1f26b374a079fba4bf564e4c0
Karan2004xd <prajapatikaran2004@gmail.com>
2023-11-17 18:01:40 +0530

    singleton finished

---
+++ Singleton/SingletonImplementation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <boost/lexical_cast.hpp>
+#include <map>
+#include <string>
+
+class SingletonDatabase {
+   std::map<std::string, int> cities;
+   
+   SingletonDatabase() {
+      std::ifstream ifs("./cites.txt");
+      
+      std::string line1, line2;
+      while (std::getline(ifs, line1)) {
+         std::getline(ifs, line2);
+         int population = boost::lexical_cast<int>(line2);
+         cities[line1] = population;
+      }
+      ifs.close();
+   }
+public:
+   SingletonDatabase(SingletonDatabase const &) = delete;
+   void operator=(SingletonDatabase const &) = delete;
+
+   static SingletonDatabase &get() {
+      static SingletonDatabase db;
+      return db;
+   }
+
+   int get_population(const std::string &city_name) {
+      return cities[city_name];
+   }
+
+};
+int main() {
+   std::string city_name {"New York City"};
+   std::cout << city_name << " has a total population of " << SingletonDatabase::get().get_population(city_name) << std::endl;
+   return 0;
+}
