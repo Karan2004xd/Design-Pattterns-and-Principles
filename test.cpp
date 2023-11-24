5edb9adb0ea23cb3f020900723f2e6219bca5839
Karan2004xd <prajapatikaran2004@gmail.com>
2023-11-24 14:37:26 +0530

    command design pattern finished

--- test.cpp
+++ test.cpp
@@ -1,81 +1,26 @@
-#include <vector>
-#include <iostream>
-
-struct Creature;
-struct Game
+struct Command
 {
-   std::vector<Creature*> creatures;
+   enum Action { deposit, withdraw } action;
+   int amount{0};
+   bool success{false};
 };
 
-struct StatQuery
+struct Account
 {
-   enum Statistic { attack, defense } statistic;
-   int result;
+   int balance{0};
 
-   StatQuery(Statistic statistic, int result) : statistic(statistic), result(result) {}
-};
-
-struct Creature
-{
-   protected:
-      Game& game;
-      int base_attack, base_defense;
-
-   public:
-      Creature(Game &game, int base_attack, int base_defense) : game(game), base_attack(base_attack),
-      base_defense(base_defense) {}
-      virtual int get_attack() = 0;
-      virtual int get_defense() = 0;
-      virtual void query(void *source, StatQuery &sq) = 0; 
-};
-
-class Goblin : public Creature
-{
-   int get_statistics(StatQuery::Statistic stat) {
-      StatQuery sq {stat, 0};
-      for (auto &i : game.creatures) {
-         i->query(this, sq);
+   void process(Command& cmd)
+   {
+      if (cmd.action == Command::Action::deposit) {
+         balance += cmd.amount;
+         cmd.success = true;
+      } else if (cmd.action == Command::Action::withdraw) {
+         if (cmd.amount <= balance) {
+            balance -= cmd.amount;
+            cmd.success = true;
+         } else {
+            cmd.success = false;
+         }
       }
-      return sq.result;
    }
-
-   public:
-      Goblin(Game &game, int base_attack, int base_defense) : Creature(game, base_attack, base_defense) {}
-
-      Goblin(Game &game) : Creature(game, 1, 1) {}
-
-      int get_attack() override {
-         return get_statistics(StatQuery::Statistic::attack);
-      }
-
-      int get_defense() override {
-         return get_statistics(StatQuery::Statistic::defense);
-      }
-
-      void query(void *source, StatQuery &sq) override {
-         if (this == source) {
-            if (sq.statistic == StatQuery::Statistic::attack) {
-               sq.result += base_attack;
-            } else if (sq.statistic == StatQuery::Statistic::defense) {
-               sq.result += base_defense;
-            }
-         } else {
-            if (sq.statistic == StatQuery::Statistic::defense) {
-               sq.result++;
-            }
-         }
-      }
-};
-
-class GoblinKing : public Goblin
-{
-   public:
-      GoblinKing(Game &game) : Goblin(game, 3, 3) {}
-      void query(void *source, StatQuery &sq) override {
-         if (sq.statistic == StatQuery::Statistic::attack && source != this) {
-            sq.result++;
-         } else {
-            Goblin::query(source, sq);
-         }
-      }
 };
